55b6a459da6181ccceea251402fcb1a45a00d976
nadirvishun <nadirvishun@gmail.com>
2018-06-15 08:06:49 +0800

    初始化时增加uploads写入权限

--- backend/config/main.php
+++ backend/config/main.php
@@ -11,7 +11,11 @@ return [
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
-    'modules' => [],
+    'modules' => [
+        'shop'=>[
+            'class' => 'backend\modules\shop\Module',
+        ]
+    ],
     'components' => [
         'request' => [
             'csrfParam' => '_csrf-backend',
